138811c68927ded1dae9fd62ec2f7e72bda44498
Tara Ayer <hitaraayer@gmail.com>
2022-06-29 09:17:18 -0500

    Initial Commit

---
+++ BossModeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BossModeApp
+//
+//  Created by Scholar on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
